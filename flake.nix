51ae933bd28e671db98acd5bed8339566d323edf
Denis Lehmann <denis@opaque.tech>
2021-04-21 18:09:23 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -50,15 +50,6 @@
               python3Packages.toml
             ];
 
-            shellHook = ''
-              if [ ! -d .venv ]; then
-                python -m venv .venv
-              fi
-              source .venv/bin/activate
-              pip install --upgrade pip
-              pip install -r requirements.txt
-            '';
-
           };
 
         }
