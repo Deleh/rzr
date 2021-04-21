3eb739800b87cee8781eea9ab6e24b87ba958fb5
Denis Lehmann <denis@opaque.tech>
2021-04-21 22:04:52 +0200

    update description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     scripts=["bin/rzr"],
     url="https://git.opaque.tech/denis/rzr",
     license="LICENSE",
-    description="Apply lightmaps to Razer devices",
+    description="A simple command line frontend for OpenRazer",
     long_description=open("README.org").read(),
     install_requires=["colour", "openrazer", "toml"],
 )
