def9dc0b3db5ac4eddf3a05cc7a9ec197830b602
Denis Lehmann <denis@opaque.tech>
2021-10-16 00:18:22 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
 {
   description = "A simple command line frontend for OpenRazer";
-
   nixConfig.bash-prompt = "\[\\e[1m\\e[32mrzr-develop\\e[0m\]$ ";
-
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
   outputs = { self, nixpkgs, flake-utils }:
-
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-
           # Package
-
           packages.rzr =
-
             pkgs.python3Packages.buildPythonApplication rec {
-
               name = "rzr";
               src = self;
-
               nativeBuildInputs = with pkgs; [
                 wrapGAppsHook
               ];
-
               propagatedBuildInputs = with pkgs; [
                 python3Packages.colour
                 python3Packages.openrazer
                 python3Packages.toml
               ];
-
             };
-
           defaultPackage = self.packages.${system}.rzr;
 
           # Development shell
-
           devShell = pkgs.mkShell {
-
             buildInputs = with pkgs; [
               python3
               python3Packages.colour
@@ -50,7 +36,6 @@
               python3Packages.toml
               python3Packages.virtualenv
             ];
-
           };
         }
       );
