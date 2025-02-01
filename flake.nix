a8dbaf61573f92265155ba71cb046ad80c51c922
Denis Lehmann <denis@opaque.tech>
2025-02-01 15:49:12 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,42 +1,50 @@
 {
   description = "A simple command line frontend for OpenRazer";
-  nixConfig.bash-prompt = "\[\\e[1m\\e[32mrzr-develop\\e[0m\]$ ";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+  outputs = { self, nixpkgs }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+      });
+    in
+    {
+      packages = forAllSystems (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = nixpkgsFor.${system};
         in
         {
-          # Package
-          packages.rzr =
-            pkgs.python3Packages.buildPythonApplication rec {
-              name = "rzr";
+          rzr =
+            pkgs.python3Packages.buildPythonPackage {
+              pname = "rzr";
+              version = "main";
               src = self;
-              nativeBuildInputs = with pkgs; [
-                wrapGAppsHook
-              ];
               propagatedBuildInputs = with pkgs; [
                 python3Packages.colour
                 python3Packages.openrazer
                 python3Packages.toml
               ];
             };
-          defaultPackage = self.packages.${system}.rzr;
+          default = self.packages.${system}.rzr;
+        }
+      );
 
-          # Development shell
-          devShell = pkgs.mkShell {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          rzr = pkgs.mkShell {
             buildInputs = with pkgs; [
               python3
               python3Packages.colour
               python3Packages.openrazer
-              python3Packages.pip
-              python3Packages.setuptools
               python3Packages.toml
-              python3Packages.virtualenv
+
             ];
           };
+          default = self.devShells.${system}.rzr;
         }
       );
+    };
 }
--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ from setuptools import setup
 
 setup(
     name="rzr",
-    version="0.0.1",
-    author="Denis Lehmann",
-    author_email="denis@opaque.tech",
-    scripts=["bin/rzr"],
-    url="https://git.opaque.tech/denis/rzr",
+    scripts=["./rzr"],
+    py_modules=[],
     license="LICENSE",
     description="A simple command line frontend for OpenRazer",
     long_description=open("README.org").read(),
